75fb38f09adfb94b3ddd76d03f0c7e35713054ac
NathanHLima <nathan1195@outlook.com>
2021-10-14 15:54:31 -0300

    exercicio de variaveis

---
+++ scripts.js
@@ -0,0 +1,12 @@
+let age = 25;
+
+console.log(age);
+
+const myName = "Nathan";
+const birthCity = "São Paulo";
+let birthYear = "1995";
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+birthYear = "2030";
+console.log(birthYear);
