bc221163890ff70884f86804664313d67d884c25
NathanHLima <nathan1195@outlook.com>
2021-10-14 20:43:54 -0300

    exercicio de switch

---
+++ pt3/pt4/scripts.js
@@ -0,0 +1,18 @@
+let estado = "";
+
+switch (estado) {
+  case "aprovada":
+    console.log("foi aprovada");
+    break;
+
+  case "lista":
+    console.log("foi para lista");
+    break;
+
+  case "reprovada":
+    console.log("foi reprovada");
+    break;
+
+  default:
+    console.log("nao se aplica");
+}
