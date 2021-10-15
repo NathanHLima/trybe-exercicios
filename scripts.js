295447d4968a04996adcb5379af0d9bed58af9fb
NathanHLima <nathan1195@outlook.com>
2021-10-15 20:25:05 -0300

    exercicio do quadrado

---
+++ scripts.js
@@ -0,0 +1,11 @@
+let n = 8;
+let inputLine = "";
+let symbol = "*";
+
+for (let index = 0; index < n; index += 1) {
+  inputLine = inputLine + symbol;
+}
+
+for (let index = 0; index < n; index += 1) {
+  console.log(inputLine);
+}
