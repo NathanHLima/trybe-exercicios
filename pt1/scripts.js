e0a9e0d0dda1d1e3aa55d6dbbccc791a2ffa4879
NathanHLima <nathan1195@outlook.com>
2021-10-15 18:39:10 -0300

    exercicios de array

---
+++ pt1/scripts.js
@@ -0,0 +1,16 @@
+// exercicio 1
+//let menu = ["Home", "Serviços", "Portfólio", "Links"];
+//let menuServices = menu[1];
+
+//console.log(menuServices);
+
+// execicio 2
+//let menu = ["Home", "Serviços", "Portfólio", "Links"];
+//let indexOfPortfolio = menu.indexOf("Portfólio");
+
+//console.log(indexOfPortfolio);
+
+// execicio 3
+let menu = ["Home", "Serviços", "Portfólio", "Links"];
+menu.push("Contato");
+console.log(menu);
