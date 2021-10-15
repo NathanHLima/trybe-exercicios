e1d4a570d8f8e0e056d5befa8637bc91a25ecfbb
NathanHLima <nathan1195@outlook.com>
2021-10-15 19:32:17 -0300

    ultima parte de exercicios

---
+++ ptfinal/scripts.js
@@ -0,0 +1,23 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let index = 0; index < numbers.length; index += 1) {
+  console.log(numbers[index]);
+}
+
+let soma = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+  soma += numbers[index];
+}
+console.log(soma);
+
+for (let index = 0; index < numbers.length; index += 1) {
+  soma += numbers[index];
+}
+let media = soma / numbers.length;
+
+if (media > 20) {
+  console.log("valor maior que 20");
+} else if (media <= 20) {
+  console.log("valor menor que 20");
+}
