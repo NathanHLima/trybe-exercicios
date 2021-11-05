cf3e3f9bf1e3875fd66d820c37f925579b14f950
NathanHLima <nathan1195@outlook.com>
2021-11-04 22:00:39 -0300

    exercicio

---
+++ aula6.1/script.js
@@ -0,0 +1,39 @@
+let todosEstados = [
+  "Acre",
+  "Alagoas",
+  "Amapá",
+  "Amazonas",
+  "Bahia",
+  "Ceará",
+  "Distrito Federal",
+  "Espírito Santo",
+  "Goiás",
+  "Maranhão",
+  "Mato Grosso",
+  "Mato Grosso do Sul",
+  "Minas Gerais",
+  "Pará",
+  "Paraíba",
+  "Paraná",
+  "Pernambuco",
+  "Piauí",
+  "Rio de Janeiro",
+  "Rio Grande do Norte",
+  "Rio Grande do Sul",
+  "Rondônia",
+  "Roraima",
+  "Santa Catarina",
+  "São Paulo",
+  "Sergipe",
+  "Tocantins",
+];
+const estados = document.querySelector("#state");
+
+for (let i = 0; i < todosEstados.length; i += 1) {
+  let estado = todosEstados[i];
+
+  let cadaEstado = document.createElement("option");
+  cadaEstado.innerText = estado;
+
+  estados.appendChild(cadaEstado);
+}
